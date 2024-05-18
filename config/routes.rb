af597cb27961a171ff00ccdff43867e6522aea7a
Juno Gu <juno.gu@kellogg.northwestern.edu>
2024-05-18 20:43:49 +0000

    240518 Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # get("/places", :controller => "places", :action => "index")
+  # get("/entries", :controller => "entries", :action => "index")
+  resources "places"
+  resources "entries"
+
+  get("/", {:controller => "things", :action => "index"})
+  
+
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
